ab69067b349a7985b18cf5e95822d6638c2cac8b
Julien Marusi <julien.marusi@bbee.fr>
2022-07-07 19:39:53 +0200

    correctif

--- Control.go
+++ Control.go
@@ -21,8 +21,6 @@ func (W *WOZFileFormat) getNextBit() byte {
 	// Lecture d'un track vide
 	// fmt.Printf("DataTrack: %v\n", W.dataTrack)
 
-	W.bitStreamPos = W.bitStreamPos % W.TRKS.Tracks[W.dataTrack].BitCount
-
 	if W.TMAP.Map[W.physicalTrack] == 0xFF {
 		W.bitStreamPos++
 		if W.bitStreamPos > 51200 {
@@ -31,6 +29,7 @@ func (W *WOZFileFormat) getNextBit() byte {
 		return byte(rand.Intn(2))
 	}
 
+	W.bitStreamPos = W.bitStreamPos % W.TRKS.Tracks[W.dataTrack].BitCount
 	targetByte := W.bitStreamPos >> 3
 	targetBit := W.bitStreamPos & 7
 
@@ -55,11 +54,11 @@ func (W *WOZFileFormat) GetNextByte() byte {
 		result |= W.getNextBit() << i
 	}
 
-	// fmt.Printf("-- [%c] T:%02.02f (%d) Pos:%d    \r", wheel[count], W.physicalTrack, W.dataTrack, W.bitStreamPos)
-	// count++
-	// if count >= len(wheel) {
-	// 	count = 0
-	// }
+	fmt.Printf("-- [%c] T:%02.02f (%d) Rev: %02d Pos:%d    \r", wheel[count], W.physicalTrack, W.dataTrack, W.revolution, W.bitStreamPos)
+	count++
+	if count >= len(wheel) {
+		count = 0
+	}
 	return result
 }
 
